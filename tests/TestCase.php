49196ccebe8f8758d4e07d57b9a74298a7efc342
Naman Malhotra <naman03malhotra@gmail.com>
2025-07-11 17:04:51 +0530

    added base test class to reset singleton

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,9 +9,27 @@ declare( strict_types=1 );
 
 namespace WooCommerce\Migrator\Tests;
 
+use ReflectionClass;
+use ReflectionException;
+
 /**
  * Our main test case class.
  */
 abstract class TestCase extends \PHPUnit\Framework\TestCase {
 
+	/**
+	 * Resets a singleton instance.
+	 *
+	 * @param string $class_name      The class name of the singleton.
+	 * @param string $property_name The name of the static property holding the instance.
+	 *
+	 * @throws ReflectionException If the class or property does not exist.
+	 */
+	protected function reset_singleton( string $class_name, string $property_name ) {
+		$reflection = new ReflectionClass( $class_name );
+		$property   = $reflection->getProperty( $property_name );
+		$property->setAccessible( true );
+		$property->setValue( null );
+		$property->setAccessible( false );
+	}
 }
